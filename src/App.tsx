e9ce1012bce903639b92839c32e30706b5576fbd
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-10-24 22:09:24 +0700

    Refactor App component to use CardNews instead of UserList

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import UserList from "./page/UserList";
+import CardNews from "./page/CardNews";
 
 function App() {
   return (
     <div className="flex justify-center items-center flex-col">
-      <h1 className="mt-5 text-3xl font-bold">👤User List</h1>
-      <UserList />
+      <h1 className="mt-5 text-3xl font-bold text-blue-600">📰 News Night</h1>
+      <CardNews />
     </div>
   );
 }
