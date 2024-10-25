d7171c03b0ad12ae14d895b9315b8b35a1fc608f
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-10-25 18:02:37 +0700

    Refactor App component to add CategoryNews and update title in CardNews

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import CardNews from "./page/CardNews";
+import CategoryNews from "./page/CategoryNews";
 
 function App() {
   return (
     <div className="flex justify-center items-center flex-col">
-      <h1 className="mt-5 text-3xl font-bold text-blue-600">📰 News Night</h1>
+      <h1 className="mt-5 text-3xl font-bold text-blue-600">News Night</h1>
+      <CategoryNews />
       <CardNews />
     </div>
   );
