d054e987728d84b6575d6c63a9e20becb0ac5662
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-11-04 09:42:51 +0700

    Fetch articles by category using CategoryContext and update loading state

--- src/page/CardNews.tsx
+++ src/page/CardNews.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useCategory } from "@/context/CategoryContext";
+
 interface Articles {
   id: string;
   name: string;
@@ -16,14 +18,18 @@ interface Articles {
 const CardNews: React.FC = () => {
   const [articles, setArticles] = useState<Articles[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const { category } = useCategory();
 
   useEffect(() => {
     const fetchArticle = async () => {
       try {
         setLoading(true);
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_NEWS}`);
+        const response = await axios.get(
+          `https://newsapi.org/v2/top-headlines?category=${category}&country=us&apiKey=${
+            import.meta.env.VITE_API_NEWS
+          }`
+        );
         setArticles(response.data.articles);
-
       } catch (error) {
         console.log("Error fetching news sources:", error);
       } finally {
@@ -31,8 +37,13 @@ const CardNews: React.FC = () => {
       }
     };
 
+    // Fetch article by category
+    if (category) {
+      fetchArticle();
+    }
+
     fetchArticle();
-  }, []);
+  }, [category]);
 
   // Proccess Loading
   if (loading) {
@@ -52,21 +63,40 @@ const CardNews: React.FC = () => {
   }
 
   return (
-    <div className="max-w-7xl min-h-screen grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 my-10 lg:mx-0 md:mx-10 mx-10">
+    <div className="max-w-7xl min-h-screen grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 my-10 lg:mx-0 md:mx-10 mx-10">
       {articles.map((article, index) => (
         <div key={index} className="border border-gray-300 rounded">
           <div className="relative">
             {article.urlToImage ? (
-              <img src={article.urlToImage} alt={article.title} className="w-full h-48 object-cover brightness-75" />
+              <img
+                src={article.urlToImage}
+                alt={article.title}
+                className="w-full h-48 object-cover brightness-75"
+              />
             ) : (
-              <img src="https://placehold.co/200x200" alt="Placeholder" className="w-full h-48 object-cover brightness-75" />
+              <img
+                src="https://placehold.co/200x200"
+                alt="Placeholder"
+                className="w-full h-48 object-cover brightness-75"
+              />
             )}
-          <h1 className="text-md font-bold absolute bottom-1 left-0 mx-2 text-white">{article.title}</h1>
+            <h1 className="text-md font-bold absolute bottom-1 left-0 mx-2 text-white">
+              {article.title}
+            </h1>
           </div>
           <div className="m-3">
-          <span className="text-md font-medium text-gray-900 mt-3">Author : {article.author}</span>
-          <p className="text-sm text-gray-500 mt-3">{article.description}</p>
-          <a href={article.url} target="_blank" rel="noreferrer" className="text-blue-500 underline font-bold mt-4">Read more</a>
+            <span className="text-md font-medium text-gray-900 mt-3">
+              Author : {article.author}
+            </span>
+            <p className="text-sm text-gray-500 mt-3">{article.description}</p>
+            <a
+              href={article.url}
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 underline font-bold mt-4"
+            >
+              Read more
+            </a>
           </div>
         </div>
       ))}
